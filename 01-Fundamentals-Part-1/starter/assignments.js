88dd0f3b8464666fcabc6dd4ebaf6f77387fe9cf
Graysen Anson <graysen.anson@gmail.com>
2021-11-01 16:43:48 -0600

    Finishing Javascript fundamental - part 1

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -169,3 +169,143 @@ console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
    8. Reflect on why we should use the === operator and type conversion in this
    situation
  */
+
+let numNeighbours = Number(prompt("How many neighbour countries does your country have?"));
+if (numNeighbours === 1) {
+   console.log("Only 1 border!");
+} else if (numNeighbours > 1) {
+   console.log("More than 1 border")
+} else {
+   console.log("No borders");
+}
+
+/**
+ * LECTURE: Logical Operators
+   1. Comment out the previous code so the prompt doesn't get in the way
+   2. Let's say Sarah is looking for a new country to live in. She wants to live in a
+   country that speaks english, has less than 50 million people and is not an
+   island.
+   3. Write an if statement to help Sarah figure out if your country is right for her.
+   You will need to write a condition that accounts for all of Sarah's criteria. Take
+   your time with this, and check part of the solution if necessary.
+   4. If yours is the right country, log a string like this: 'You should live in Portugal :)'. If
+   not, log 'Portugal does not meet your criteria :('
+   5. Probably your country does not meet all the criteria. So go back and temporarily
+   change some variables in order to make the condition true (unless you live in
+   Canada :D)
+ */
+
+let isIsland = false;
+let speech = 'English';
+let pop = '51000000';
+if (isIsland == false && speech === 'English' && pop < 50000000) {
+   console.log("Canada is for you.")
+} else {
+   console.log("Canada is not right for you.")
+}
+
+////////////////////////////////////
+// Coding Challenge #3
+
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
+
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+
+TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+GOOD LUCK 😀
+*/
+
+let d1 = 97;
+let d2 = 108;
+let d3 = 89;
+let dAVG = (d1 + d2 + d3) / 3;
+let k1 = 88;
+let k2 = 91;
+let k3 = 110;
+let kAVG = (k1 + k2 + k3) / 3;
+
+if (dAVG > kAVG && dAVG >= 100) {
+   console.log("dolphines win.");
+} else if (dAVG < kAVG && kAVG >= 100) {
+   console.log("Koalas win.");
+} else if (dAVG === kAVG && dAVG >= 100 && kAVG >= 100) {
+   console.log("Both teams tie");
+} else {
+   console.log("No one wins the trophy");
+}
+
+/**
+ *LECTURE: The switch Statement
+   1. Use a switch statement to log the following string for the given 'language':
+   chinese or mandarin: 'MOST number of native speakers!'
+   spanish: '2nd place in number of native speakers'
+   english: '3rd place'
+   hindi: 'Number 4'
+   arabic: '5th most spoken language'
+   for all other simply log 'Great language too :D'
+ */
+lang = 'English';
+switch (lang) {
+   case 'Chinese':
+   case 'Mandarin':
+      console.log("Most number of native speakers.");
+      break;
+   case 'Spanish':
+      console.log("2nd place in number of native spearks");
+      break;
+   case 'Hindi':
+      console.log("Number 4");
+      break;
+   case 'Arabic':
+      console.log("5th most spoken language");
+      break;
+   default:
+      console.log("Great language too :D");
+}
+
+/**
+ * LECTURE: The Conditional (Ternary) Operator
+   1. If your country's population is greater than 33 million, use the ternary operator
+   to log a string like this to the console: 'Portugal's population is above average'.
+   Otherwise, simply log 'Portugal's population is below average'. Notice how only
+   one word changes between these two sentences!
+   2. After checking the result, change the population temporarily to 13 and then to
+   130. See the different results, and set the population back to original
+ */
+pop2 = 32000000;
+
+pop2 > 33000000 ? console.log("Portugals population is above average") :
+   console.log("Portugal's population is below average.");
+
+////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Steven wants to build a very simple tip calculator for whenever he goes eating in a resturant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+1. Your task is to caluclate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: 'The bill was 275, the tip was 41.25, and the total value 316.25'
+
+TEST DATA: Test for bill values 275, 40 and 430
+
+HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+
+GOOD LUCK 😀
+*/
+
+const totalBill = 250;
+const tip = totalBill * 0.15;
+
+totalBill >= 50 && totalBill <= 300 ? console.log("Your Bill is: $" + totalBill + " Tip is: $" + tip + " And your final total is: $" + (totalBill + tip)) :
+   console.log("Bill is either lower than 50 or greater than 300");
+
+/** Fundementals part 2 */
