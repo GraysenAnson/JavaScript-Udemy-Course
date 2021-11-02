7ca0ab08af28a4e091b865fed6584035d08ec3ef
Graysen Anson <graysen.anson@gmail.com>
2021-11-02 10:37:19 -0600

    Javascript section 2 number 35 done

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,11 @@
+`use strict`; // Didn't work with my run code extension??.
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+    hasDriversLicense = true;
+}
+if (hasDriversLicense) {
+    console.log('I can drive :)');
+}
