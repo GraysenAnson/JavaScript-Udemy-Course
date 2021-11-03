0778a1575b89157cba25c8589b45ea73ff036c0a
Graysen Anson <graysen.anson@gmail.com>
2021-11-02 18:57:41 -0600

    Coding challange 2 done

--- 02-Fundamentals-Part-2/starter/assessments.js
+++ 02-Fundamentals-Part-2/starter/assessments.js
@@ -161,6 +161,42 @@ if (neighbors1[0] != 'Germany' || neighbors1[1] != 'Germany' || neighbors1[2] !=
 neighbors1[1] = 'U S FUCKING A.';
 console.log(neighbors1);
 
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the 
+rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! 
+So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+GOOD LUCK 😀
+*/
+const bills = [125, 555, 44];
+const tip1 = [];
+const total = [];
+
+function calcTip(bill) {
+   if (bill >= 50 && bill <= 300) {
+      tip1.push(bill * 0.20);
+      total.push(bill + (bill * 0.20));
+   } else {
+      tip1.push(bill * 0.15);
+      total.push(bill + (bill * 0.15));
+   }
+}
+calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]);
+console.log(tip1);
+console.log(total);
+
 /**
  * LECTURE: Introduction to Objects
 1. Create an object called 'myCountry' for a country of your choice, containing
